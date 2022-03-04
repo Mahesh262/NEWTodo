121d4bd9f9650ab886768265e7113e986ca21a7e
Mahesh262 <maheshilluri262@gmail.com>
2022-03-04 18:02:58 +0530

    first-Commit

--- src/Bcompnt.jsx
+++ src/Bcompnt.jsx
@@ -25,6 +25,7 @@ const Bcompnt = (props) => {
                                 </td>
                             </tr>
                         ))
+                        
                     ) :
                         (
                             <tr>
